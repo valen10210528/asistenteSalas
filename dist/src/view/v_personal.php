57515471cf44aa494500961a6637a2c5de8a13a7
valenunicatolica <130628861+valenunicatolica@users.noreply.github.com>
2023-10-15 14:22:42 -0500

    Update v_personal.php

--- dist/src/view/v_personal.php
+++ dist/src/view/v_personal.php
@@ -1,13 +1,9 @@
 <div class="row">
 	<div class="col-xl-12 ps-xl-12">
 		<div class="row">
-			<!--begin::Col-->
 			<div class="col col-sm-12">
-				<!--begin::Engage widget 1-->
 			<H1>BIENVENIDOS</H1>
 			</div>
 		</div>
 	</div>
-
-
 </div>
