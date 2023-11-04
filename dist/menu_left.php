56258a7f6a6a37950393ac3ea9919729a722d835
valenunicatolica <valentina.duque01@unicatolica.edu.co>
2023-11-04 14:46:44 -0500

    prueba valentina

--- dist/menu_left.php
+++ dist/menu_left.php
@@ -5,7 +5,7 @@
 	<div id="kt_aside_menu_wrapper" class="w-100 hover-scroll-overlay-y scroll-ps d-flex" data-kt-scroll="true" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_aside_logo, #kt_aside_footer" data-kt-scroll-wrappers="#kt_aside, #kt_aside_menu" data-kt-scroll-offset="0" style="padding-top: 350px;">
 		<div id="kt_aside_menu" class="menu menu-column menu-title-gray-600 menu-state-primary menu-state-icon-primary menu-state-bullet-primary menu-icon-gray-500 menu-arrow-gray-500 fw-bold fs-6" data-kt-menu="true">
 			<div class="menu-item py-3" data-step="3" data-intro="Clic en el modulo deseado para ingresar o desplegar las opciones complementarias">
-				<a class="menu-link active" href="?url_id=personal" title="Inicio" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss="click" data-bs-placement="right" onclick=cargando()>
+				<a class="menu-link active" href="?url_id=agenda_reservas" title="Inicio" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss="click" data-bs-placement="right" onclick=cargando()>
 					<span class="menu-icon" style="background-color: #FFC300">
 						<!--begin::Svg Icon | path: icons/duotune/general/gen025.svg-->
 						<span class="svg-icon svg-icon-2x">
