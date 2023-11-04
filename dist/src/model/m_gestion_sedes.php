448e1fc37dd67427879781350dac5fd24fd5b734
valenunicatolica <valentina.duque01@unicatolica.edu.co>
2023-11-03 22:39:30 -0500

    INSERT

--- dist/src/model/m_gestion_sedes.php
+++ dist/src/model/m_gestion_sedes.php
@@ -3,13 +3,47 @@
 setlocale(LC_TIME, "spanish");
 date_default_timezone_set('America/Bogota');
 
+// $dbm = conectar_mysql();
+// $error_personal = null;
+// $fecha_hoy = date("Y-m-d");
+// $fecha_hora_hoy = date("Y-m-d H:i:s"); //fecha_creacion
+// $formulario = variable_exterior("formulario");
+
 $dbm = conectar_mysql();
-$error_personal = null;
-$fecha_hoy = date("Y-m-d");
-$fecha_hora_hoy = date("Y-m-d H:i:s"); //fecha_creacion
-$formulario = variable_exterior("formulario");
+$formulario = $_POST['formulario'];
+
+
+if ($formulario == "crear_sede") {
+
+    $nombre_sede = $_POST['nombre_sede'];
+    $direccion_sede = $_POST['direccion_sede'];
+
+    $sql = "INSERT INTO sedes (nombre, estado, direccion) VALUES ('$nombre_sede', '1', '$direccion_sede')";
+    $query = $dbm->prepare($sql);
+    if ($query->execute()) {
+        // EJECUTÓ BIEN
+?>
+        <script>
+            alert("La sede se insertó correctamente");
+        </script>
+
+    <?php
+    } else {
+        // ERROR
+    ?>
+        <script>
+            alert("Error! La sede no se insertó correctamente");
+        </script>
+
+<?php
+    }
+}
+
+
+
 
 $consulta = "";
+
 
 
 
